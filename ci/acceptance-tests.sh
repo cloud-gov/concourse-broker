15ab797656de5f65107d90bb8c1455bae879f35f
James C. Scott <jamesc.scott@gsa.gov>
2017-01-05 10:59:21 -0500

    Add basic acceptance tests

--- ci/acceptance-tests.sh
+++ ci/acceptance-tests.sh
@@ -11,16 +11,36 @@ space_guid=$(cf space "${CF_SPACE}" --guid)
 
 # Test concourse ci plan
 
+# Try to find team before creation and should find nothing
+
+RESULT=$(curl -L "${CONCOURSE_URL}/api/v1/teams" -s | jq -c ".[] | select(.name | contains (\"${CF_ORGANIZATION}\"))")
+if [ ! -z "$RESULT" ]; then
+    echo "Found concourse team before creation. Failing for now."
+    exit 1
+fi
+
 # Create service instance
 cf create-service concourse-ci "${PLAN_NAME}" "${SERVICE_INSTANCE_NAME}"
 instance_guid=$(cf service "${SERVICE_INSTANCE_NAME}" --guid)
 
-# TODO Run actual tests
+
+# Try to find team after service creation and should find the list.
+
+RESULT=$(curl -L "${CONCOURSE_URL}/api/v1/teams" -s | jq -c ".[] | select(.name | contains (\"${CF_ORGANIZATION}\"))")
+if [ -z "$RESULT" ]; then
+    echo "Could not find concourse team after service creation. Failing..."
+    exit 1
+fi
 
 # Delete service instance
 cf delete-service -f "${SERVICE_INSTANCE_NAME}"
 
-# TODO Check if service was really deleted.
+
+RESULT=$(curl -L "${CONCOURSE_URL}/api/v1/teams" -s | jq -c ".[] | select(.name | contains (\"${CF_ORGANIZATION}\"))")
+if [ ! -z "$RESULT" ]; then
+    echo "Found concourse team after deletion. Failing..."
+    exit 1
+fi
 
 ####
 
