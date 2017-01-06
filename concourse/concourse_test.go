a58eb5c85356fa2f412bbce568c06df5102c3f04
James C. Scott <jamesc.scott@gsa.gov>
2017-01-06 10:06:19 -0500

    use logger and add getauthtests

--- concourse/concourse_test.go
+++ concourse/concourse_test.go
@@ -1,6 +1,7 @@
 package concourse
 
 import (
+	"code.cloudfoundry.org/lager"
 	"github.com/18F/concourse-broker/cf"
 	"github.com/18F/concourse-broker/config"
 	"github.com/concourse/atc"
@@ -18,7 +19,7 @@ var _ = Describe("Concourse", func() {
 					AdminUsername: "user",
 					AdminPassword: "password",
 				}
-				client := NewClient(env)
+				client := NewClient(env, logger)
 				expectedClient := new(concourseClient)
 				Expect(client).Should(BeAssignableToTypeOf((expectedClient)))
 			})
@@ -43,8 +44,6 @@ var _ = Describe("Concourse", func() {
 				ID:   1,
 				Name: "team venture",
 			}
-
-			team = client.Team("team venture")
 		})
 		Context("when I create a team successfully", func() {
 			BeforeEach(func() {
@@ -69,9 +68,10 @@ var _ = Describe("Concourse", func() {
 				)
 			})
 			It("returns no error", func() {
-				client := NewClient(env)
+				client := NewClient(env, logger)
 				err := client.CreateTeam(cf.Details{OrgName: "team venture"})
 				Expect(err).NotTo(HaveOccurred())
+				Expect(logger.Logs()).To(HaveLen(0))
 			})
 		})
 		Context("when I try to create a team with a name that is used for an existing team", func() {
@@ -90,10 +90,17 @@ var _ = Describe("Concourse", func() {
 				)
 			})
 			It("should fail and indicate it could not provision", func() {
-				client := NewClient(env)
+				client := NewClient(env, logger)
 				err := client.CreateTeam(cf.Details{OrgName: "team venture"})
 				Expect(err).To(HaveOccurred())
 				Expect(err.Error()).To(ContainSubstring("Team team venture already exists"))
+				logs := logger.Logs()
+				Expect(logs).To(HaveLen(1))
+				Expect(logs[0].LogLevel).To(Equal(lager.ERROR))
+				Expect(logs[0].Message).To(ContainSubstring("concourse-client.create-team.existing-team-error"))
+				Expect(logs[0].Data["team-name"]).To(Equal("team venture"))
+				Expect(logs[0].Data["auth-methods-size"]).To(BeEquivalentTo(1))
+
 			})
 
 		})
@@ -120,9 +127,33 @@ var _ = Describe("Concourse", func() {
 				)
 			})
 			It("returns an error", func() {
-				client := NewClient(env)
+				client := NewClient(env, logger)
 				err := client.CreateTeam(cf.Details{OrgName: "team venture"})
 				Expect(err).To(HaveOccurred())
+				logs := logger.Logs()
+				Expect(logs).To(HaveLen(1))
+				Expect(logs[0].LogLevel).To(Equal(lager.ERROR))
+				Expect(logs[0].Message).To(ContainSubstring("concourse-client.create-team.unknown-create-error"))
+				Expect(logs[0].Data["team-name"]).To(Equal("team venture"))
+			})
+		})
+		Context("when I try to delete a team but I can't auth as an admin", func() {
+			BeforeEach(func() {
+				atcServer.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/api/v1/teams/main/auth/token"),
+						ghttp.RespondWithJSONEncoded(http.StatusUnauthorized, nil),
+					),
+				)
+			})
+			It("returns an error", func() {
+				client := NewClient(env, logger)
+				err := client.CreateTeam(cf.Details{OrgName: "team venture"})
+				Expect(err).To(HaveOccurred())
+				logs := logger.Logs()
+				Expect(logs).To(HaveLen(1))
+				Expect(logs[0].LogLevel).To(Equal(lager.ERROR))
+				Expect(logs[0].Message).To(ContainSubstring("concourse-client.create-team.auth-client-error"))
 			})
 		})
 
@@ -150,9 +181,10 @@ var _ = Describe("Concourse", func() {
 				)
 			})
 			It("returns no error", func() {
-				client := NewClient(env)
+				client := NewClient(env, logger)
 				err := client.DeleteTeam(cf.Details{OrgName: "team venture"})
 				Expect(err).NotTo(HaveOccurred())
+				Expect(logger.Logs()).To(HaveLen(0))
 			})
 		})
 		Context("when Concourse blows up when trying to delete an instance", func() {
@@ -171,9 +203,34 @@ var _ = Describe("Concourse", func() {
 				)
 			})
 			It("returns an error stating 'couldn't destroy team'", func() {
-				client := NewClient(env)
+				client := NewClient(env, logger)
 				err := client.DeleteTeam(cf.Details{OrgName: "team venture"})
 				Expect(err).To(HaveOccurred())
+				logs := logger.Logs()
+				Expect(logs).To(HaveLen(1))
+				Expect(logs[0].LogLevel).To(Equal(lager.ERROR))
+				Expect(logs[0].Message).To(ContainSubstring("concourse-client.delete-team.unknown-delete-error"))
+				Expect(logs[0].Data["team-name"]).To(Equal("team venture"))
+			})
+		})
+
+		Context("when I try to delete a team but I can't auth as an admin", func() {
+			BeforeEach(func() {
+				atcServer.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/api/v1/teams/main/auth/token"),
+						ghttp.RespondWithJSONEncoded(http.StatusUnauthorized, nil),
+					),
+				)
+			})
+			It("returns an error", func() {
+				client := NewClient(env, logger)
+				err := client.DeleteTeam(cf.Details{OrgName: "team venture"})
+				Expect(err).To(HaveOccurred())
+				logs := logger.Logs()
+				Expect(logs).To(HaveLen(1))
+				Expect(logs[0].LogLevel).To(Equal(lager.ERROR))
+				Expect(logs[0].Message).To(ContainSubstring("concourse-client.delete-team.auth-client-error"))
 			})
 		})
 
